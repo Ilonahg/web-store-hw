7dcc7dafe0df366cdc5f86668b1423af7a960ecc
ilonahg <gogilchyn2005ilona@gmail.com>
2025-07-01 12:27:51 +0300

    обавлены регистрация, авторизация, настройка почтового сервера

--- catalog/models.py
+++ catalog/models.py
@@ -1,11 +1,29 @@
 from django.db import models
+from django.conf import settings
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    STATUS_CHOICES = [
+        ('draft', 'Черновик'),
+        ('published', 'Опубликован'),
+    ]
+
+    name = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    is_available = models.BooleanField(default=True)
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='draft'
+    )
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+
+    class Meta:
+        permissions = [
+            ('can_unpublish_product', 'Может отменять публикацию продукта'),
+        ]
 
     def __str__(self):
         return self.name
--- settings.py
+++ settings.py
@@ -109,3 +109,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+# Настройки почтового сервера
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'django20055@gmail.com'  # ← здесь укажи свою почту
+EMAIL_HOST_PASSWORD = 'ybmh wvnz uorg isf '  # ← пароль приложения из Gmail
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
